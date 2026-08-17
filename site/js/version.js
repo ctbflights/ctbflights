6717b9dc2c6f7c77571e9ddaa5cd9594e3df06f6
fnf010906-sys <fnf010906@gmail.com>
2026-08-17 22:01:32 +0800

    bump CTB Flights to v1.1.3

--- site/js/version.js
+++ site/js/version.js
@@ -1,6 +1,6 @@
 (() => {
   'use strict';
-  const VERSION = '1.1.2';
+  const VERSION = '1.1.3';
   window.CTB_VERSION = VERSION;
   document.querySelectorAll('[data-version]').forEach(el => { el.textContent = `v${VERSION}`; });
   const baseTitle = document.body?.dataset?.page === 'results'
